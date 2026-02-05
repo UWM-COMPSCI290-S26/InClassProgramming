0b021447d56db16a73eb80b493ed5d64347be71a
jeraldlt <jeraldlt@uwm.edu>
2026-02-05 11:23:25 -0600

    Interpolation

--- main.py
+++ main.py
@@ -26,6 +26,31 @@ def draw_line_1(surf, x0, x1, y0, y1, color):
         if check_line(Point(x + 1, y + 0.5), Point(x0, y0), Point(x1,  y1)) < 0:
             y += 1
 
+def draw_line_gradient(surf, a, b, color_a, color_b):
+    y = a.y
+    for x in range(a.x, b.x):
+        denom = (a.x - b.x)
+        if denom == 0.0:
+            t_x = 1.0
+        else:
+            t_x = (a.x - x) / (a.x - b.x)
+        denom = (a.y - b.y)
+        if denom == 0.0:
+            t_y = 1.0
+        else:
+            t_y = (a.y - y) / (a.y - b.y)
+
+        t = (t_x + t_y) / 2.0
+
+        red = color_a[0] * (1 - t) + color_b[0] * t
+        green = color_a[1] * (1 - t) + color_b[1] * t
+        blue = color_a[2] * (1 - t) + color_b[2] * t
+
+        surf.set_at((x, y), (red, green, blue))
+
+        if check_line(Point(x + 1, y + 0.5), a, b) < 0:
+            y += 1
+
 def draw_triangle(surf, a, b, c, color):
     min_x = min(a.x, b.x, c.x)
     max_x = max(a.x, b.x, c.x)
@@ -34,6 +59,12 @@ def draw_triangle(surf, a, b, c, color):
 
     for x in range(min_x, max_x):
         for y in range(min_y, max_y):
+            if check_line(Point(x, y), a, b) < 0:
+                continue
+            if check_line(Point(x, y), b, c) < 0:
+                continue
+            if check_line(Point(x, y), c, a) < 0:
+                continue
             surf.set_at((x, y), color)
 
 
@@ -55,8 +86,9 @@ while running:
 
     #draw_line_1(screen, 0, 500, 250, 250, (0, 0, 0))
 
-    draw_triangle(screen, Point(100, 100), Point(300, 200), Point(150, 350), (0, 0, 0))
+    #draw_triangle(screen, Point(100, 100), Point(300, 200), Point(150, 350), (0, 0, 0))
 
+    draw_line_gradient(screen, Point(0, 250), Point(500, 250), (0, 0, 0,), (255, 0, 0))
     pygame.display.update()
 
 # Quit Pygame
